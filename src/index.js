b48b196075f1d1ef094b16025ce3b25f2d09886b
Caerdroia <denis.voronin@gmail.com>
2022-01-04 23:41:31 +0300

    refactor: code format

--- src/index.js
+++ src/index.js
@@ -9,22 +9,22 @@ module.exports = function toReadable (number) {
     let result = '';
     if (number === 0) return 'zero';
     for (i = 0; i < array.length; i++) {
-                  if (i === 0) {
+        if (i === 0) {
             result = `${result} ${arraySingleDigits[array[i]]}`;
-          }
-        if(i === 1) {
-                   if (array[1] != 1) {
-            result = `${result} ${arrayTens[array[i]]}`;
-            } else {
+            }
+        else if(i === 1) {
+            if (array[1] != 1) {
+                result = `${result} ${arrayTens[array[i]]}`;
+            }else{
                 result = `${arrayTeenDigits[array[i-1]]}`;
             }
         } 
-             else if (i === 2) {
+        else if (i === 2) {
             result = `${result} ${bigNumbers[0]} ${arraySingleDigits[array[i]]}`;
-                        }
-                      }
-            return result.split(' ').reverse().join(' ').replace(/\s+/g, ' ').trim();
-              }
+            }
+        }
+        return result.split(' ').reverse().join(' ').replace(/\s+/g, ' ').trim();
+        }
 
 
 
