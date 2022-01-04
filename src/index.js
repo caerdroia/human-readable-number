8a7cf36b46924f9cc178cd92be89e14e5c900332
Caerdroia <denis.voronin@gmail.com>
2022-01-04 21:44:18 +0300

    refactor: change toString for String

--- src/index.js
+++ src/index.js
@@ -4,34 +4,27 @@ module.exports = function toReadable (number) {
     const arrayTeenDigits = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
     const arrayTens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];   
     const bigNumbers = ['hundred'];
-        
-    const array = number.toString().split('').reverse();
+    const array = String(number).split('').reverse();
     
     let result = '';
     if (number === 0) return 'zero';
-        
     for (i = 0; i < array.length; i++) {
                   if (i === 0) {
             result = `${result} ${arraySingleDigits[array[i]]}`;
           }
-    
         if(i === 1) {
-            
-            if (Number(array[1]) !== 1) {
+                   if (Number(array[1]) !== 1) {
             result = `${result} ${arrayTens[array[i]]}`;
-
             } else {
                 result = `${arrayTeenDigits[array[i-1]]}`;
             }
         } 
-        else if (i === 2) {
+             else if (i === 2) {
             result = `${result} ${bigNumbers[0]} ${arraySingleDigits[array[i]]}`;
                         }
                       }
-
             return result.split(' ').reverse().join(' ').replace(/\s+/g, ' ').trim();
-            
-        }
+              }
 
 
 
